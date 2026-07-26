4b4c9372bf8c5e6d58ca7c8b1d9c8a839ca2cdba
Rahul Moharana <moharanarahul466@gmail.com>
2026-07-26 07:13:56 +0530

    added

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from "react";
-import { Analytics } from "@vercel/analytics/next"
+import { Analytics } from "@vercel/analytics/react";
 import { BrowserRouter as Router, Routes, Route, Outlet } from "react-router-dom";
 import gsap from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
